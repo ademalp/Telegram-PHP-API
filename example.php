3f44b988c11636a9ce3bd93afcde9a357d096560
ademalp <ademalp@gmail.com>
2022-02-11 11:53:51 +0300

    Delete example.php

--- example.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$token ="5290839350:AAEm78yPS4v4-nS4YPdBahTi7zuWPnP0iKw";
-include 'telebot.php';
-$teleBot = new TeleBotLib($token);
-$chatId = $teleBot->findChatId();
-$result = $teleBot->sendMessage($chatId, "Test Mesaj");
-var_export($result);
